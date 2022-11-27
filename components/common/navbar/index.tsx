4389f9cb12c493da186d6315045e6939d6a40ff1
EONCreator <itcyborg2001@gmail.com>
2022-11-27 07:49:23 +0500

    navbar fixes

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 const NavBar = () => (
-		<nav class="navbar shadow px-3 w-full flex items-center justify-between">
+		<nav class="navbar px-3 w-full flex items-center justify-between">
         <a class="items-center space-x-2">
             <svg className="logo" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  
 			stroke-linecap="round"  stroke-linejoin="round">  <line x1="3" y1="12" x2="21" y2="12" />  <line x1="3" y1="6" x2="21" y2="6" />  
