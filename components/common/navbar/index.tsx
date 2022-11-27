579a49c629c662dc91cb28352a521466c3c8ce17
EONCreator <itcyborg2001@gmail.com>
2022-11-27 07:38:57 +0500

    navbar fixed

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -6,7 +6,7 @@ const NavBar = () => (
             <svg className="logo" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  
 			stroke-linecap="round"  stroke-linejoin="round">  <line x1="3" y1="12" x2="21" y2="12" />  <line x1="3" y1="6" x2="21" y2="6" />  
 			<line x1="3" y1="18" x2="21" y2="18" /></svg>
-            <div>Панель управления</div></a>
+            <div className="control-label">Панель управления</div></a>
 
             <div class="items-block">
                 <div class="blocks">
